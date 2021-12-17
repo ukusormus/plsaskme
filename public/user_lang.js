4e5219aa0018000178db173bd9421b6daceeb7e3
3xploit-hunt <sormus.uku@gmail.com>
2021-12-18 01:06:30 +0200

    Closer...

--- public/user_lang.js
+++ public/user_lang.js
@@ -29,7 +29,7 @@ const spicyText = document.querySelector("#spicy-section > h4");
 const langText = document.querySelector("#lang-section > h4");
 const darkText = document.querySelector("#darkmode-section > h4")
 
-spicyText.textContent = dict.language["en"];
+// spicyText.textContent = dict.language["en"];
 
 const currentQ = document.querySelector("#current-q");
 let langSpicy = localStorage.getItem("lang-spicy");
