2233322c80444c2c0e46ed2cf901540cc44552a2
3xploit-hunt <sormus.uku@gmail.com>
2021-12-18 01:43:52 +0200

    ...

--- public/user_lang.js
+++ public/user_lang.js
@@ -100,27 +100,27 @@ let langSpicy = localStorage.getItem("lang-spicy");
 
 
 // Language changed
-langSwitch.addEventListener("click", () => {
-    const newLang = langSwitch.checked ? "et" : "en";
+// langSwitch.addEventListener("click", () => {
+//     const newLang = langSwitch.checked ? "et" : "en";
 
-    // Update switch texts
-    spicyText.textContent = dict.spicy[newLang];
-    langText.textContent = dict.language[newLang];
-    darkText.textContent = dict.lightdark[newLang]
+//     // Update switch texts
+//     spicyText.textContent = dict.spicy[newLang];
+//     langText.textContent = dict.language[newLang];
+//     darkText.textContent = dict.lightdark[newLang]
 
-    // Update currentQ
-    currentQ.textContent = dict.tapMe[newLang];
+//     // Update currentQ
+//     currentQ.textContent = dict.tapMe[newLang];
 
-    // Update local storage
-    const currentSpicy = spicySwitch.checked ? "-spicy" : "";
-    localStorage["lang-spicy"] = newLang + currentSpicy;
-});
+//     // Update local storage
+//     const currentSpicy = spicySwitch.checked ? "-spicy" : "";
+//     localStorage["lang-spicy"] = newLang + currentSpicy;
+// });
 
-// Spiciness changed
-spicySwitch.addEventListener("click", () => {
-    const newSpicy = spicySwitch.checked ? "-spicy" : "";
-    const currentLang = langSwitch.checked ? "et" : "en";
+// // Spiciness changed
+// spicySwitch.addEventListener("click", () => {
+//     const newSpicy = spicySwitch.checked ? "-spicy" : "";
+//     const currentLang = langSwitch.checked ? "et" : "en";
 
-    // Update local storage
-    localStorage["lang-spicy"] = currentLang + newSpicy;
-});
+//     // Update local storage
+//     localStorage["lang-spicy"] = currentLang + newSpicy;
+// });
