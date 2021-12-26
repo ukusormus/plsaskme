c761746bef57944cda1ab9d3ef1c7743a6c5b313
3xploit-hunt <sormus.uku@gmail.com>
2021-12-26 23:31:57 +0200

    mobile landscape hmm

--- public/swipe.js
+++ public/swipe.js
@@ -51,7 +51,7 @@ if (touchSupported) {
         change = change / window.innerHeight * 100;
 
         currTopCard.style.transform = `translate(-50%, ${-50 - change}%) scale(${1 - (change * 0.005)})`;
-        currTopCard.style.opacity = `${1 - (change * 0.005)}`;
+        // currTopCard.style.opacity = `${1 - (change * 0.005)}`;
         // currTopCard.style.filter = `brightness(${0.95 - (change * 0.005)})`;
 
         newTopCard.style.setProperty("--after-opacity", `${change / 100}`);
