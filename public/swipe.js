8fb4b8a50a06850ea9bcfbf3efd033695a95acb4
3xploit-hunt <sormus.uku@gmail.com>
2021-12-26 16:19:00 +0200

    Swiping (no new question yet)

--- public/swipe.js
+++ public/swipe.js
@@ -1,15 +1,28 @@
-let curr = document.querySelector("body > main > div.q-container")
+let currTopCard;
+let newTopCard;
 
-curr.addEventListener("touchstart", start)
-curr.addEventListener("touchmove", move)
-curr.addEventListener("touchend", end)
+function init() {
+
+    currTopCard = document.querySelector(".q-container.draggable");
+    newTopCard = document.querySelector(".q-container:not(.draggable)");
+
+    currTopCard.addEventListener("touchstart", start);
+    currTopCard.addEventListener("touchmove", move);
+    currTopCard.addEventListener("touchend", end);
+}
+
+
+init();
 
 let startingY = newY = 0;
-const threshold = window.innerHeight / 4;
+const threshold = window.innerHeight / 5.5;
 
 function start(e) {
+    currTopCard.style.transition = ``;
     startingY = e.touches[0].clientY;
-    console.log("Touch started", startingY)
+    console.log("Touch started", startingY);
+
+    newTopCard.style.opacity = 1;
 }
 
 function move(e) {
@@ -17,14 +30,17 @@ function move(e) {
 
     let change = startingY - newY;
 
-    console.log("Touch moving, change:", change)
+    console.log("Touch moving, change:", change);
 
     if (change <= 0) return;
 
     change = change / window.innerHeight * 100
 
-    curr.style.transform = `translate(-50%, ${-50 - change}%) scale(${1 - (change * 0.005)})`
-    curr.style.opacity = `${1 - (change * 0.01)}`
+    currTopCard.style.transform = `translate(-50%, ${-50 - change}%) scale(${1 - (change * 0.005)})`
+    currTopCard.style.opacity = `${1 - (change * 0.005)}`
+
+    newTopCard.style.setProperty("--after-opacity", `${change / 100}`);
+
     e.preventDefault();
 }
 
@@ -33,10 +49,33 @@ function end(e) {
 
     if (startingY - newY < threshold) {
         // reset position
-        curr.style.transform = `translate(-50%, -50%)`
-        curr.style.opacity = `1`
+        currTopCard.style.transition = `transform 0.1s`;
+        currTopCard.style.transform = `translate(-50%, -50%)`;
+        currTopCard.style.opacity = `1`;
+        newTopCard.style.setProperty("--after-opacity", "0");
     } else {
-        // move, next q etc
-        curr.style.opacity = 0;
+        // Make the card <div.q-container.draggable> #current-q </div> fly away
+        currTopCard.style.transform = "";
+        currTopCard.classList.add("flyaway");
+
+        newTopCard.classList.add("draggable");
+        newTopCard.style.setProperty("--after-opacity", "1");
+
+        // If transition ended (has flown away), make the new one draggable
+        currTopCard.addEventListener("transitionend", transition);
+
+        function transition() {
+
+            currTopCard.classList.remove("draggable", "flyaway");
+            currTopCard.style.opacity = 0;
+
+            currTopCard.removeEventListener("touchstart", start);
+            currTopCard.removeEventListener("touchmove", move);
+            currTopCard.removeEventListener("touchend", end);
+
+            currTopCard.removeEventListener("transitionend", transition)
+
+            init();
+        }
     }
 }
